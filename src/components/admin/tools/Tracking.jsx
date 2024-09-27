ccae428eed233830bdc64449bd08406d5a7f2e5e
Deepak Dhiman <dhimandeepak957@gmail.com>
2024-09-27 16:56:50 +0530

    tracking tool

--- src/components/admin/tools/Tracking.jsx
+++ src/components/admin/tools/Tracking.jsx
@@ -8,49 +8,56 @@ import createAxiosInstance from "../../../config/axiosConfig";
 import book1Img from "../../../assets/rdnumber.png";
 import { toastifyError } from "../../../constants/errors";
 
-const Tracking = ({ open = false, setOpen = () => {} }) => {
+const Tracking = ({ open = false, setOpen = () => { } }) => {
     const handleOpen = () => setOpen(!open);
     const [showSpinner, setShowSpinner] = useState(false);
     const axios = createAxiosInstance();
 
     const book1 = useRef(null);
-    
+    const excelNameRef = useRef(null);
+
     const startMerge = async () => {
         const file1 = book1.current.files[0];
+        const excelName = excelNameRef.current.value;
 
-        console.log(file1);
-        if(file1){
-            const formData = new FormData();
-            formData.append("book1", file1);
-
-            setShowSpinner(true);
-            try{
-                const response = await axios.postForm("/tools/bulkTrackPdfsAndExcel", formData, {
-                    responseType: 'blob'
-                });
-                // console.log(response);
-                if(response.status === 200){
+        // console.log(file1);
+        if (file1) {
+            if(excelName && excelName !== ""){
+                const formData = new FormData();
+                formData.append("book1", file1);
+                formData.append("excelName", excelName);
+    
+                setShowSpinner(true);
+                try {
+                    const response = await axios.postForm("/tools/bulkTrackPdfsAndExcel", formData, {
+                        responseType: 'blob'
+                    });
+                    // console.log(response);
+                    if (response.status === 200) {
+                        setShowSpinner(false);
+                        const blob = response.data;
+                        const url = window.URL.createObjectURL(blob);
+                        const a = document.createElement('a');
+                        a.style.display = 'none';
+                        a.href = url;
+                        a.download = 'trackingBook.xlsx';
+                        document.body.appendChild(a);
+                        a.click();
+                        window.URL.revokeObjectURL(url);
+                        toastify({ msg: "File merged and downloaded successfully!", type: "success" })
+                        book1.current.value = "";
+                        handleOpen();
+                    }
+                } catch (err) {
+                    console.log("error:", err)
+                    toastifyError(err);
+                } finally {
                     setShowSpinner(false);
-                    const blob = response.data;
-                    const url = window.URL.createObjectURL(blob);
-                    const a = document.createElement('a');
-                    a.style.display = 'none';
-                    a.href = url;
-                    a.download = 'trackingBook.xlsx';
-                    document.body.appendChild(a);
-                    a.click();
-                    window.URL.revokeObjectURL(url);
-                    toastify({ msg: "File merged and downloaded successfully!", type: "success" })
-                    book1.current.value = "";
-                    handleOpen();
                 }
-            }catch(err) {
-                console.log("error:",err)
-                toastifyError(err);
-            }finally {
-                setShowSpinner(false);
+            }else {
+                toastify({ msg: "Please fill in the Excel name field.", type: "error" });
             }
-        }else {
+        } else {
             toastify({ msg: "No Book File Selected", type: "error" })
         }
     }
@@ -74,13 +81,17 @@ const Tracking = ({ open = false, setOpen = () => {} }) => {
 
                     <div className="bg-white flex flex-col py-5 px-2 gap-y-7 w-full">
                         <div className="flex justify-start gap-x-2 items-center">
-                            <label htmlFor="book1" className="font-poppins not-talic cursor-pointer text-[16px] transition-all hover:text-slate-600 leading-normal font-bold text-slate-700">Book1:</label>
-                            <input ref={book1} type="file" id="book1" name="book1" />
+                            <div className="flex justify-start items-start flex-col gap-y-3">
+                                <label htmlFor="book1" className="font-poppins not-talic cursor-pointer text-[16px] transition-all hover:text-slate-600 leading-normal font-bold text-slate-700">Book1:</label>
+                                <input ref={book1} type="file" id="book1" name="book1" />
+                                <label htmlFor="excelName" className="font-poppins not-talic cursor-pointer text-[16px] transition-all hover:text-slate-600 leading-normal font-bold text-slate-700">Excel Name:</label>
+                                <input ref={excelNameRef} type="text" id="excelName" name="excelName" className="outline-none border border-solid border-gray-950 rounded-sm py-1 px-2" />
+                            </div>
                             <img src={book1Img} alt="image" className="w-32 border border-solid border-slate-300 shadow-md shadow-slate-600" />
                         </div>
 
                         <Button className="bg-rose-700 py-2 text-[16px] rounded-sm flex justify-center items-center w-full capitalize font-poppins not-italic leading-normal gap-x-2 font-medium" onClick={startMerge}>
-                            { showSpinner ? <Spinner width={16} /> : null }
+                            {showSpinner ? <Spinner width={16} /> : null}
                             Start Tracking
                         </Button>
                     </div>
